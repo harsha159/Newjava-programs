f2ac9d0ca1a7066eabb305fc42361e9096ae02ac
harsha <harsha4d6@gmail.com>
2021-05-27 00:29:11 +0530

    please approve

---
+++ src/java123/Interface.java
@@ -0,0 +1,14 @@
+package java123;
+
+public class Interface {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static void mkpewngpn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
